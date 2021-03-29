3fc81274332f8c308646ef17e0109e7e7789856c
gabieller <gabieller@hotmail.com>
2021-03-29 10:52:48 -0300

    adding files

--- task01/task01/src/App.js
+++ task01/task01/src/App.js
@@ -48,11 +48,4 @@ class App extends Component {
 
 export default App;
 
-// Add a method to manipulate the state (=> an event-handler method)
-// Pass the event-handler method reference to the UserInput component and bind it to the input-change event
-// Ensure that the new input entered by the user overwrites the old username passed to UserOutput
-// Add two-way-binding to your input (in UserInput) to also display the starting username
-// Add styling of your choice to your components/ elements in the components - both with inline styles and stylesheets
 
-//Onchange
-//changes on inpuy
