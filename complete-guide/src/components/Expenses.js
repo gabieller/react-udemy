290f0d98984a221d28554016c74e5adda89c2916
gabieller <gabriela.eller@indicium.tech>
2021-04-23 20:29:46 -0300

    new course

--- complete-guide/src/components/Expenses.js
+++ complete-guide/src/components/Expenses.js
@@ -12,6 +12,10 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear)
   }
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear
+  })
+
   return (
     <div>
       <Card className="expenses">
@@ -19,7 +23,7 @@ const Expenses = (props) => {
           onChangeFilter={filterChangedHandler}
           selected={filteredYear}
         />
-        {props.items.map((expense) => (
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
             key={expense.id}
             title={expense.title}
